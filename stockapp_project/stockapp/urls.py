6f21c9a5a439e96f39a2a1025a9d7e056614b03a
prabhjot kaur <99008608+prabh78371@users.noreply.github.com>
2022-04-28 13:45:21 +0530

    Update urls.py

--- stockapp_project/stockapp/urls.py
+++ stockapp_project/stockapp/urls.py
@@ -4,9 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stockapi/',views.stock_api,name="stockApi"),
-    path('stockapi/<int:product_id>',views.stock_api,name="stockApi"),
-    path('productapi/',views.product_api,name="productApi"),
+    path('Products/',views.Products,name="productapi"),
+    path('Products/<int:barcode_number>',views.Products,name="productapi"),
  
 
 
