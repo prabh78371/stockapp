ac3fecd34e02351ebb2e9a9f6daf295ad4a1a90d
prabhjot kaur <99008608+prabh78371@users.noreply.github.com>
2022-04-28 13:48:46 +0530

    fix(Products view)

--- stockapp_project/stockapp/views.py
+++ stockapp_project/stockapp/views.py
@@ -5,47 +5,18 @@ from django.shortcuts import render
 from itsdangerous import Serializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import Productserilizer,Transactionserilizer,categoryserilizer,Inventoryserilizer
+from .serializers import Productserilizer,Transactionserilizer,Categoryserilizer,Inventoryserilizer
 from .models import Category, Product,Transaction,Inventory
 from rest_framework import status
 
-# Create your views here.
-@api_view(['GET','POST'])
-def stock_api(request,product_id=None):
+@api_view(['GET'])
+def Products(request,barcode_number=None):
     if request.method ==  'GET':
-        id=product_id
+        id = barcode_number
         if id is not None:
-            prod = Inventory.objects.get(product_id=id)
-            serilizer = Inventoryserilizer(prod)
+            prod = Product.objects.get(barcode_number=id)
+            serilizer = Productserilizer(prod)
             return Response(serilizer.data)
-        prod = Inventory.objects.all()
-        serilizer = Inventoryserilizer(prod,many=True)
+        prod = Product.objects.all()
+        serilizer = Productserilizer(prod,many=True,)
         return Response(serilizer.data)
-
-    if request.method == 'POST':
-        serilizer = Inventory(data = request.data)
-        if serilizer.is_valid():
-            serilizer.save()
-            return Response(serilizer.data,status = status.HTTP_201_CREATED)
-        return Response(serilizer.errors,status = status.HTTP_400_BAD_REQUEST)
-
-    
-
-@api_view(['GET','POST'])
-def product_api(request,pk=None):
-    if request.method ==  'GET':
-        id = pk
-        if id is not None:
-            prod = Transaction.objects.get(id=id)
-            serilizer = Transactionserilizer(prod)
-            return Response(serilizer.data)
-        prod = Transaction.objects.all()
-        serilizer = Transactionserilizer(prod,many=True)
-        return Response(serilizer.data)
-
-    if request.method == 'POST':
-        serilizer = Transactionserilizer(data = request.data)
-        if serilizer.is_valid():
-            serilizer.save()
-            return Response(serilizer.data,status = status.HTTP_201_CREATED)
-        return Response(serilizer.errors,status = status.HTTP_400_BAD_REQUEST)
