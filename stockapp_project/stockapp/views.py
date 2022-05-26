8101ff6da7e37400108357feb9a056fdb8b362cf
prabhjot kaur <99008608+prabh78371@users.noreply.github.com>
2022-05-26 13:48:06 +0530

    add(product,category,transaction,inventoery api)

--- stockapp_project/stockapp/views.py
+++ stockapp_project/stockapp/views.py
@@ -9,8 +9,8 @@ from .serializers import Productserilizer,Transactionserilizer,Categoryserilizer
 from .models import Category, Product,Transaction,Inventory
 from rest_framework import status
 
-@api_view(['GET'])
-def Products(request,barcode_number=None):
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def products(request,barcode_number=None):
     if request.method ==  'GET':
         id = barcode_number
         if id is not None:
@@ -19,4 +19,105 @@ def Products(request,barcode_number=None):
             return Response(serilizer.data)
         prod = Product.objects.all()
         serilizer = Productserilizer(prod,many=True,)
+        return Response(serilizer.data,status = status.HTTP_200_OK)
+
+    
+    if request.method == 'POST':
+        serilizer = Productserilizer(data = request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status = status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        id = barcode_number
+        prod = Product.objects.get(barcode_number=id)
+        serilizer = Productserilizer(prod,data = request.data )
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status=status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status=status.HTTP_400_BAD_REQUEST)  
+
+    if request.method == 'PATCH':
+        id = barcode_number
+        prod = Product.objects.get(barcode_number=id)
+        serilizer = Productserilizer(prod,data = request.data,partial =True )
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status=status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status=status.HTTP_400_BAD_REQUEST)  
+
+    if request.method == 'DELETE':
+        id = barcode_number
+        prod = Product.objects.get(barcode_number=id)
+        prod.delete()
+        return Response({})
+
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def category(request,pk=None):
+    if request.method ==  'GET':
+        id = pk
+        if id is not None:
+            cat = Category.objects.get(id=id)
+            serilizer = Categoryserilizer(cat)
+            return Response(serilizer.data)
+        cat = Category.objects.all()
+        serilizer = Categoryserilizer(cat,many=True,)
         return Response(serilizer.data)
+  
+    if request.method == 'POST':
+        serilizer = Categoryserilizer(data = request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status = status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        id = pk
+        cat = Category.objects.get(id=id)
+        serilizer = Categoryserilizer(cat,data = request.data )
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status=status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status=status.HTTP_400_BAD_REQUEST)  
+
+    if request.method == 'PATCH':
+        id = pk
+        cat = Category.objects.get(id=id)
+        serilizer = Categoryserilizer(cat,data = request.data,partial =True )
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status=status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status=status.HTTP_400_BAD_REQUEST)  
+
+    if request.method == 'DELETE':
+        id = pk
+        prod = Category.objects.get(id=id)
+        prod.delete()
+        return Response({})
+
+@api_view(['GET'])
+def inventory(request):
+    if request.method ==  'GET':
+        prod = Inventory.objects.all()
+        serilizer = Inventoryserilizer(prod,many=True,)
+        return Response(serilizer.data)
+
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def transaction(request,pk=None):
+    if request.method ==  'GET':
+        id = pk
+        if id is not None:
+            trans = Transaction.objects.get(id=id)
+            serilizer = Transactionserilizer(trans)
+            return Response(serilizer.data)
+        cat = Transaction.objects.all()
+        serilizer = Transactionserilizer(cat,many=True,status = status.HTTP_200_OK)
+        return Response(serilizer.data,status = status.HTTP_400_BAD_REQUEST)
+  
+    if request.method == 'POST':
+        serilizer = Transactionserilizer(data = request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status = status.HTTP_201_CREATED)
+        return Response(serilizer.errors,status = status.HTTP_400_BAD_REQUEST)
