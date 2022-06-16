8821ec32186788e19d36d0319400c6e7fbad1762
prabhjot kaur <99008608+prabh78371@users.noreply.github.com>
2022-06-16 14:18:05 +0530

    update Productserilizer and Categoryserilizer  fields

--- stockapp_project/stockapp/serializers.py
+++ stockapp_project/stockapp/serializers.py
@@ -4,16 +4,18 @@ from .models import Product,Inventory,Category,Transaction
 from rest_framework import serializers
        
 class categoryserilizer(serializers.ModelSerializer):
+    # product = serializers.HiddenField(default = 1)
     class Meta:
         model = Category
-        fields = ['product_code','packs_per_carton']
+        fields = ['product','product_code','packs_per_carton']
         depth = 1
         
 class Productserilizer(serializers.ModelSerializer):
+    barcode_number = serializers.HiddenField(default = 1)
     category_set = Categoryserilizer(many=True,read_only=True)
     class Meta:
         model = Product
-        ffields = ['name','category_set']
+        ffields = ['barcode_number','name','category_set']
 
 class Inventoryserilizer(serializers.ModelSerializer):
     class Meta:
